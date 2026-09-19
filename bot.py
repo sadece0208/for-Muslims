c0503ccfde386e8e5c1326deb141ffd9ac39c42e
sadece0208 <mukhammadsadeecow@gmail.com>
2026-09-19 06:30:20 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,21 @@
+import os
+import threading
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def health_check():
+  return "Bot ishlamoqda!", 200
+
+
+def run_http():
+  port = int(os.environ.get("PORT", 10000))
+  app.run(host="0.0.0.0", port=port)
+
+
+threading.Thread(target=run_http, daemon=True).start()
 import logging
 import random
 from datetime import time
